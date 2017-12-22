85077608f5f6a65a445d8bc78198f4731d969d89
Lowdinggg <lowdinggg@gmail.com>
2017-12-22 16:17:47 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+  FILE* f = fopen(argv[1], "wb");
+  fwrite("\x50\x4B\x05\x06\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 22, 1, f);
+  fclose(f);
+
+}
