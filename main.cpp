56f013f256103127d4f4a0efd4c9d1a7890d8cc7
Lowdinggg <lowdinggg@gmail.com>
2017-12-22 16:27:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,7 @@ int main(int argc, char** argv) {
   FILE* f = fopen(argv[1], "wb");
   fwrite("\x50\x4B\x05\x06\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0", 22, 1, f);
   fclose(f);
+  std::cout << "Successfully created zip file: " << argv[1] << std::endl;
+  return 0;
 
 }
